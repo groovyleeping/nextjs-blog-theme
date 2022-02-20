e5f02f2cb480cd01df224a61d6aeb1a71b3d5218
John Nicholson <John.Nicholson@core-uk.com>
2022-02-20 09:41:16 +0000

    changed blog vars to Atomic Stocks

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Atomic Stocks';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Atomic Stocks';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'Atomic Stocks';
 
   return {
     name,
